64f504b606b47c66eef1875993456387c9267620
MarekStudenciak <260363@student.pwr.edu.pl>
2023-06-28 21:42:46 +0200

    Revert "Dodanie countera uderzeń"
    This reverts commit fcd7d3250946769b890e5125ee93130451cb9e1f.

--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -11,12 +11,10 @@ public class Controller : MonoBehaviour
     [SerializeField] private Slider slider;
     [SerializeField] private LineRenderer ballFront;
     [SerializeField] private TextMeshProUGUI shootText;
-    [SerializeField] private TextMeshProUGUI shootCounter;
     private bool shot = false;
     private Vector3 lineStartVector;
     private Vector3 lineEndVector;
     private float zTime;
-    private int shots = 0;
 
     void Start()
     {
@@ -26,7 +24,6 @@ public class Controller : MonoBehaviour
     }
     private void Update()
     {
-        shootCounter.text = shots.ToString();
         RaycastHit hit;
         Vector3 vec = ballFront.transform.TransformPoint(lineStartVector - new Vector3(0, 0, 0.1f));
         if (Physics.Raycast(vec, ballFront.transform.forward, out hit, (lineEndVector - lineStartVector).magnitude * shotStrength))
@@ -94,9 +91,8 @@ public class Controller : MonoBehaviour
     {
         rb.AddForce(1000f * Mathf.Pow(shotStrength + 1, 2) * shotStrength * ballFront.transform.forward);
         shot = true;
-        shots +=1;
         ballFront.enabled = false;
-        shootText.text = "STRZA≈Å";
+        shootText.text = "STRZA£";
     }
     private void Stop()
     {
